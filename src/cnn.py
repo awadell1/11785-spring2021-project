a3bfdde967dfb36561657e51dac0cb6912c5aca2
Beatrice Lee <beatricelee@beatrices-mbp.myfiosgateway.com>
2021-04-21 16:04:48 -0400

    CNN first pass

--- src/cnn.py
+++ src/cnn.py
@@ -8,6 +8,9 @@ import time
 from dataset import *
 
 
+#modified code from: 
+
+
 class conv(nn.Module):
     def __init__(self,in_ch,out_ch):
         super(conv,self).__init__()
@@ -22,18 +25,19 @@ class conv(nn.Module):
 class inconv(nn.Module):
     def __init__(self,in_ch,out_ch):
         super(inconv,self).__init__()
-        self.inconv=nn.Sequential(
+        self.conv=nn.Sequential(
             nn.Conv3d(in_ch,out_ch,3,padding=1),
             nn.BatchNorm3d(out_ch),
             nn.PReLU(),
         )
     def forward(self, x):
-        x=self.inconv(x)
+        x=self.conv(x)
         return x
 class res_block(nn.Module):
+    ''''''
     def __init__(self,in_ch,out_ch,d=1):
         super(res_block,self).__init__()
-        self.conv = nn.Sequential(
+        self.doubleconv = nn.Sequential(
             nn.Conv3d(in_ch, out_ch, (3, 3, 1), padding=(d, d, 0), dilation=(d, d, 1)),
             nn.BatchNorm3d(out_ch),
             nn.PReLU(),
@@ -42,19 +46,20 @@ class res_block(nn.Module):
             nn.PReLU()
         )
     def forward(self, x):
-        x1 = self.conv(x)
-        x =x+x1
+        x1 = self.doubleconv(x)
+        x =x+ x1
         return x
 class anistropic_conv(nn.Module):
+    '''1X1X3'''
     def __init__(self,in_ch,out_ch):
         super(anistropic_conv,self).__init__()
-        self.anistropic_conv=nn.Sequential(
+        self.aniconv=nn.Sequential(
             nn.Conv3d(in_ch,out_ch,(1,1,3),padding=(0,0,1),dilation=1),
             nn.BatchNorm3d(out_ch),
             nn.PReLU(),
         )
     def forward(self,x):
-        x1=self.anistropic_conv(x)
+        x1=self.aniconv(x)
         return x1
 class Block_3(nn.Module):
     def __init__(self,in_ch,out_ch,flag):
@@ -125,11 +130,13 @@ class up(nn.Module):
             self.conv = nn.Sequential(
                 nn.Conv3d(in_ch, out_classes, (3, 3, 1), padding=(1, 1, 0)),
             )
+
+
     def forward(self, x):
         x=self.conv(x)
         return x
 class WNET(nn.Module):
-    def __init__(self,n_channels,out_ch,n_classes):
+    def __init__(self,n_channels,out_ch,n_classes, dice=hyper["dice"]):
         super(WNET,self).__init__()
         self.conv=conv(n_channels,out_ch)
         self.block0=Block_2(out_ch,out_ch,0)
@@ -140,6 +147,8 @@ class WNET(nn.Module):
         self.up1 = up(out_ch, n_classes*2, 4)
         self.up2 = up(out_ch, n_classes*4, 4)
         self.out=up(7*n_classes,n_classes,1)
+        self.dice = dice
+
     def forward(self, x):
         x=self.conv(x)
         x=self.block0(x)
@@ -151,71 +160,128 @@ class WNET(nn.Module):
         x=self.up2(x)
         x=torch.cat([x0,x1,x],dim=1)
         x=self.out(x)
-        return F.sigmoid(x)
+        x= F.sigmoid(x)
+        # if self.dice:
+        #   return x, torch.argmax(x,axis=1)
+        return x, torch.argmax(x,axis=1)
+
 class ENET(nn.Module):
-    def __init__(self,n_channels,out_ch,n_classes):
+    def __init__(self,n_channels,out_ch,n_classes,dice=hyper["dice"]):
         super(ENET, self).__init__()
         self.conv = conv(n_channels, out_ch)
         self.block0 = Block_2(out_ch, out_ch, 1)
         self.block1 = Block_2(out_ch, out_ch, 1)
         self.block2 = Block_3(out_ch, out_ch, 1)
         self.block3 = Block_3(out_ch, out_ch, 0)
+        self.up0 = up(out_ch, n_classes, 1)
         self.up1 = up(out_ch, n_classes * 2, 2)
         self.up2 = up(out_ch, n_classes * 2, 2)
         self.out = up(5 * n_classes, n_classes, 1)
+        self.dice = dice
 
     def forward(self, x):
         x = self.conv(x)
         x,_ = self.block0(x)
         x0, x = self.block1(x)
+        x0 = self.up0(x0)
         x = self.block2(x)
         x1 = self.up1(x)
         x = self.block3(x)
         x = self.up2(x)
         x = torch.cat([x0, x1, x], dim=1)
         x = self.out(x)
-        return F.sigmoid(x)
+        x= F.sigmoid(x)
+        return x, torch.argmax(x,axis=1)
 
-def get_loaders(batchSize, train, val=None, test=None):
+
+def get_loaders(batchSize, train_set, val_set, test_set):
 
   print("*** Create data loader ***")
   # Train
   train_loader_args = dict(shuffle=True, batch_size=batchSize, num_workers=8)
-  train_loader = DataLoader(Brats2017(train), **train_loader_args)
+  train_loader = DataLoader(train_set, **train_loader_args)
   
-  if val == None and test==None:
+  if val_set == None and test_set==None:
     return train_loader
     
   # Dev
-  dev_loader = DataLoader(Brats2017(val), **train_loader_args)
+  dev_loader = DataLoader(val_set, **train_loader_args)
   
   # Test
   test_loader_args = dict(shuffle=False, batch_size=batchSize, num_workers=8)
-  test_loader = DataLoader(Brats2017(test), **test_loader_args)
+  test_loader = DataLoader(test_set, **test_loader_args)
   
   return train_loader, dev_loader, test_loader
 
+def evaluate(models, data_set, criterion, epoch):
+  label_values = [1,2,4]
+  start_time = time.time()
+  with torch.no_grad():
+    for batch_idx, (data, target) in enumerate(data_set):
+      data, original_target = data.cuda(), target.cuda()
+      for i in range(len(models)):
+        target = (original_target==label_values[i]).type(torch.cuda.FloatTensor)
+        data=data.type(torch.cuda.FloatTensor)
+        model = models[i]
+        if i>0:
+          data=torch.unsqueeze(data,axis=1)
+        # optimizer = optimizers[i]
+        # optimizer.zero_grad()
+        if hyper["dice"]:
+          out, data = model(data)
+        else:
+          out, data = model(data)        
+        if hyper["dice"]:
+          loss = criterion(data,target) 
+        else:
+          loss = criterion(out.type(torch.cuda.FloatTensor), target.type(torch.int64))        
+      dice = Dice(data,target)
+      if batch_idx % 50 == 0:
+          print("Evaluating Epoch: {}\tBatch: {}\tTimestamp: {}\tLoss: {}\tDice:{}".format(epoch, batch_idx, time.time() - start_time, loss, dice))   
+      # if batch_idx > 2:  
+      #   break
+      torch.cuda.empty_cache()
+
+
 def train_epoch(models, data_set, criterion, optimizers, epoch):
   start_time = time.time()
+  label_values = [1,2,4]
+
   for batch_idx, (data, target) in enumerate(data_set):
-    data, target = data.cuda(), target.cuda()
-    #since we are using dataset for now
-    data =torch.unsqueeze(data,0)
-    #[TODO] target needs to extract binding box
+    # if batch_idx>=0:
+    #   break
+    data, original_target = data.cuda(), target.cuda()
+    # data.requires_grad_(True)
     for i in range(len(models)):
+      
       model = models[i]
       optimizer = optimizers[i]
       optimizer.zero_grad()
-      data = model(data)
-      #[TODO] output needs to extract binding box
-      loss = criterion(data,target) 
+      if i>0:
+        data = target
+        data=torch.unsqueeze(data,axis=1)
+      # data=torch.unsqueeze(data,axis=0)
+      if hyper["dice"]:
+        out, data = model(data)
+      else:
+        out, data = model(data)
+      data = data.type(torch.cuda.FloatTensor)
+      target = (original_target==label_values[i]).type(torch.cuda.FloatTensor)
+      data.requires_grad_(True)
+      target.requires_grad_(True)
+      if hyper["dice"]:
+        loss = criterion(data,target) 
+      else:
+        loss = criterion(out.type(torch.cuda.FloatTensor), target.type(torch.int64))
       loss.backward()
       optimizer.step()
       if batch_idx % 50 == 0:
-          print("Epoch: {}\tBatch: {}\tTimestamp: {}".format(epoch, batch_idx, time.time() - start_time))     
+          print("Epoch: {}\tBatch: {}\tTimestamp: {}\tLoss: {}".format(epoch, batch_idx, time.time() - start_time, loss))     
       torch.cuda.empty_cache()
-      del data
-      del target
+    # if batch_idx > 1:
+    #   break
+    
+  evaluate(models, dev_loader ,criterion, epoch)
 
 
 class DiceLoss(nn.Module):
@@ -223,23 +289,47 @@ class DiceLoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super(DiceLoss, self).__init__()
 
-    def forward(self, inputs, targets, smooth=1):
+    def forward(self, inputs, targets, smooth=2):
+
+        # assert(inputs.shape==targets.shape)
         
         #comment out if your model contains a sigmoid or equivalent activation layer
-        # inputs = F.sigmoid(inputs)       
         
         #flatten label and prediction tensors
         inputs = inputs.view(-1)
         targets = targets.view(-1)
+
+
+        #Getting weirdly large sum of input. Predicting stuff as 1.
+        # print("Sum of input:{}, targets:{}".format(inputs.sum(), targets.sum()))
         
         intersection = (inputs * targets).sum()                            
         dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
         
         return 1 - dice
 
+def Dice(inputs, targets, smooth=2):
+
+  # assert(inputs.shape==targets.shape)
+  
+  #comment out if your model contains a sigmoid or equivalent activation layer
+  
+  #flatten label and prediction tensors
+  inputs = inputs.view(-1)
+  targets = targets.view(-1)
+
+  #Getting weirdly large sum of input. Predicting stuff as 1.
+  # print("Sum of input:{}, targets:{}".format(inputs.sum(), targets.sum()))
+  
+  intersection = (inputs * targets).sum()                            
+  dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+  
+  return dice
+
+
 hyper = {
   "dataPath": "./data_root",
-  "batchSize": 1,
+  "batchSize": 5,
   "lr":1e-3,
   "weightDecay":1e-7,
   "nEpochs":20,
@@ -247,37 +337,51 @@ hyper = {
   "seed":20,
   "load_model":False,
   "c_o":32,
-  "c_l":2
+  "c_l":2,
+  "dice":False
 }
 
 def main():
-  np.random.seed(hyper["seed"])
-  torch.manual_seed(hyper["seed"])
-  torch.cuda.manual_seed(hyper["seed"])
+    #Create data loaders
+    train_path = "data/Brats17TrainingData"
+    val_path = "data/Brats17TrainingData"
+    test_path = "data/Brats17TrainingData"
 
-  print("*** Create the model and define Loss and Optimizer ***")
+    dataset = Brats2017(train_path)
+    train_set, val_set, test_set = dataset.split_dataset(train_path)
+    train_loader, dev_loader, test_loader = get_loaders(5, train_set, val_set, test_set)
 
-  wnet = WNET(144, hyper["c_o"], hyper["c_l"])
-  tnet = WNET(hyper["c_o"],hyper["c_o"], hyper["c_l"])
-  enet = ENET(hyper["c_o"],hyper["c_o"], hyper["c_l"])
+    np.random.seed(hyper["seed"])
+    torch.manual_seed(hyper["seed"])
+    torch.cuda.manual_seed(hyper["seed"])
 
-  wnet, tnet, enet = wnet.cuda(), tnet.cuda(), enet.cuda()
-  models = [wnet, tnet, enet]
+    print("*** Create the model and define Loss and Optimizer ***")
 
-  # checkpoint = torch.load(hyper["savedCheckpoint"])
-  # model.load_state_dict(checkpoint["model_state_dict"])
-  optimizer_w = optim.Adam(wnet.parameters(), lr=hyper["lr"], weight_decay=hyper["weightDecay"])
-  optimizer_t = optim.Adam(tnet.parameters(), lr=hyper["lr"], weight_decay=hyper["weightDecay"])
-  optimizer_e = optim.Adam(enet.parameters(), lr=hyper["lr"], weight_decay=hyper["weightDecay"])
+    wnet = WNET(4, hyper["c_o"], hyper["c_l"])
+    tnet = WNET(1,hyper["c_o"], hyper["c_l"])
+    enet = ENET(1,hyper["c_o"], hyper["c_l"])
 
-  optimizers = [optimizer_w, optimizer_t, optimizer_e]
+    wnet, tnet, enet = wnet.cuda(), tnet.cuda(), enet.cuda()
+    models = [wnet, tnet, enet]
 
-  criterion = DiceLoss()
+    # checkpoint = torch.load(hyper["savedCheckpoint"])
+    # model.load_state_dict(checkpoint["model_state_dict"])
+    optimizer_w = optim.Adam(wnet.parameters(), lr=hyper["lr"], weight_decay=hyper["weightDecay"])
+    optimizer_t = optim.Adam(tnet.parameters(), lr=hyper["lr"], weight_decay=hyper["weightDecay"])
+    optimizer_e = optim.Adam(enet.parameters(), lr=hyper["lr"], weight_decay=hyper["weightDecay"])
 
-  # Train the model for N epochs
-  for i in range(hyper["nEpochs"]):
+    optimizers = [optimizer_w, optimizer_t, optimizer_e]
 
-    # Train
-    print("Train\tEpoch: {}".format(i))
-    startTime = time.time()
-    train_epoch(models, train_set, criterion, optimizers, i)
+    if hyper["dice"]:
+        criterion = DiceLoss()
+    else:
+        criterion = nn.CrossEntropyLoss()
+
+    # Train the model for N epochs
+    for i in range(hyper["nEpochs"]):
+
+        # Train
+        print("Train\tEpoch: {}".format(i))
+        startTime = time.time()
+        train_epoch(models, train_loader, criterion, optimizers, i)
+
