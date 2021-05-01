Fix gradient issue with Adversary

src/gan.py
@@ -281,8 +281,8 @@ class LiBrainTumorSegAdv(nn.Module):
                     nonlinearity="leaky_relu",
                 )
             elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
+                nn.init.normal_(m.weight.data, 1.0, 0.02)
+                nn.init.constant_(m.bias.data, 0)
 
     def forward(self, mri_patch, mri_segment):
         # Predicts 1 iff mri_segment is real
@@ -335,7 +335,6 @@ class CNNBlock(nn.Module):
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
             elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
     def forward(self, x):

tests/test_LiBrainGan.py
@@ -6,7 +6,7 @@ from src.util import SparseDiceLoss
 
 def test_segmentor():
     seg = LiBrainTumorSegGen()
-    input = torch.rand((1, 4, 31, 31))
+    input = torch.rand((1, 4, 31, 31), requires_grad=False)
     out, logits = seg(input)
 
     # Check shape
@@ -18,14 +18,33 @@ def test_segmentor():
     expected = torch.ones((out.shape))
     assert all(torch.isclose(logit_sums, expected).flatten())
 
+    # Check loss gradient
+    target = torch.randint(0, 5, (1, 15, 15), requires_grad=False)
+    loss = seg.loss(out, target)
+    loss.backward()
+    total_norm = 0
+    for p in seg.parameters():
+        total_norm += p.grad.norm() ** 2
+    assert total_norm.sqrt() > 0.1
+
 
 def test_adversary():
     adv = LiBrainTumorSegAdv()
-    mri_patch = torch.rand((3, 4, 31, 31))
-    labels = torch.rand((3, 5, 15, 15))
+    mri_patch = torch.rand((3, 4, 31, 31), requires_grad=True)
+    labels = torch.rand((3, 5, 15, 15), requires_grad=False)
 
     out = adv(mri_patch, labels)
+    adv_label = torch.tensor([0.0, 0.0, 0.0], requires_grad=False)
+    loss = adv.loss(out, adv_label)
     assert list(out.shape) == [3]
+
+    # Check loss gradient
+    loss.backward()
+    total_norm = 0
+    for p in adv.parameters():
+        total_norm += p.grad.norm() ** 2
+    assert total_norm.sqrt() > 0.1
+    assert mri_patch.grad.norm() > 0.1
 
 
 def test_dice():